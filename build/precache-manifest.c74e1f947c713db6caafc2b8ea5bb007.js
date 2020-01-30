c2f77ab4dc60703a0b1fd9030214f69f873dd39a
na8esin <na8esin.mpst@gmail.com>
2020-01-30 04:37:21 +0000

    build

---
+++ build/precache-manifest.c74e1f947c713db6caafc2b8ea5bb007.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a286b65adf638491d3e2397f61f59087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50c0a500097db96a8202",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "0b3f2d3c0c310dfeb4e1",
+    "url": "/static/js/2.cdf0e46f.chunk.js"
+  },
+  {
+    "revision": "50c0a500097db96a8202",
+    "url": "/static/js/main.6418ad0f.chunk.js"
+  },
+  {
+    "revision": "2c78ea4909e735898380",
+    "url": "/static/js/runtime~main.ac5c9a11.js"
+  }
+]);
